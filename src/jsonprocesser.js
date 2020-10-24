7bfe61835eed515a4136d805de83a0f72b5f39ca
Jeffrey Huang <jeffreyzt.huang@mail.utoronto.ca>
2020-10-24 19:42:53 -0400

    added json processor

--- src/jsonprocesser.js
+++ src/jsonprocesser.js
@@ -3,14 +3,27 @@
 export default function processJSON(geojson){
     const features = geojson['features']
     const low = {'type': 'FeatureCollection', 'features': []}
-    const medium ={'type': 'FeatureCollection', 'features': []}
+    const medium = {'type': 'FeatureCollection', 'features': []}
     const high = {'type': 'FeatureCollection', 'features': []} 
     const ohno = {'type': 'FeatureCollection', 'features': []}
+    console.log(low['features'])
     for (let i = 0; i < features.length ; i++){
-        console.log(features[i])
+        const aqi = features[i]['properties']['aqi']
+        if (aqi <= 50){
+            low['features'].push(features[i])
+        }
+        else if (aqi <= 100){
+            medium['features'].push(features[i])
+        }
+        else if (aqi <= 150){
+            high['features'].push(features[i])
+        
+        }
+        else{
+            ohno['features'].push(features[i])
+        }
     }
-
+    return [low, medium, high, ohno]
     // geojson = {'type': 'FeatureCollection', 'features': features}
-    console.log(features)
 
 }
