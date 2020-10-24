91582da1d8dc8164633a8560e6a316ada4471f2b
Danny Oh <dannyoh0316@gmail.com>
2020-10-24 12:50:20 -0500

    markers showing

--- api/api.py
+++ api/api.py
@@ -4,19 +4,19 @@ import json
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/api', methods=['GET'])
 def api():
     states = {}     
     cities = ["colorado", "new+york", "maine"]
     for city in cities:
         thingsWeWant = {}           # [geo, aqi, name]
         response = req.get("https://api.waqi.info/search/?token=491989f2467ee8ad41a1ed72d33635bf9669f1e6&keyword=" + city)
+        print(response.json())
         data = response.json()
-        for oneData in data["data"]:
-            aqi, geo, name = oneData["aqi"], oneData["station"]["geo"], oneData["station"]["name"]
-            tempDict = {}
-            tempDict["aqi"], tempDict["geo"], tempDict["name"] = aqi, geo, name
-            thingsWeWant[oneData["uid"]] = tempDict
-        states[city] = thingsWeWant
-    return states
-    
+        # for oneData in data["data"]:
+        #     aqi, geo, name = oneData["aqi"], oneData["station"]["geo"], oneData["station"]["name"]
+        #     tempDict = {}
+        #     tempDict["aqi"], tempDict["geo"], tempDict["name"] = aqi, geo, name
+        #     thingsWeWant[oneData["uid"]] = tempDict
+        # states[city] = thingsWeWant
+    return data
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,17 +1,18 @@
 import React, {useState} from 'react';
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, {Marker} from 'react-map-gl';
 import mapboxgl from 'mapbox-gl';
 
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZGFubnlvaDAzMTYiLCJhIjoiY2tnbjF5enpiMDV3azJ5cWxzcWd5djJ6NCJ9.fN9v1ZMyAVCSIWeITwhg7w';
 
-const Map = () => {
+const Map = ({apiData}) => {
   const [viewport, setViewport] = useState({
     latitude: 40.8069488,
     longitude: -73.9618974,
     width: '100vw',
     height: '100vh',
-    zoom: 15
+    // zoom: 15
+    zoom: 1
   });
 
   return (
@@ -22,6 +23,17 @@ const Map = () => {
       mapStyle='mapbox://styles/mapbox/dark-v10'
       onViewportChange={nextViewport => setViewport(nextViewport)}
     >
+      {apiData.data.map(d =>
+        (
+          <Marker
+            key={d.uid}
+            latitude={Number(d.station.geo[0])}
+            longitude={Number(d.station.geo[1])}
+          >
+            <img src='../marker.png' height='3%' width='3%' />
+          </Marker>
+        )
+      )}
     </ReactMapGL>
   );
 };
