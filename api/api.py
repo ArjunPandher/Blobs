b5314df340d020ed6d85c3a2b7986a111f25979e
Danny Oh <dannyoh0316@gmail.com>
2020-10-24 00:18:10 -0500

    react and flask clashing

--- api/api.py
+++ api/api.py
@@ -7,12 +7,20 @@ app = Flask(__name__)
 @app.route('/api', methods=['GET'])
 def api():
     # given "cities"
+    #[cities]
+
+    # results[]
     # for city in cities
-        # response = req.get("https://api.waqi.info/search/?token=491989f2467ee8ad41a1ed72d33635bf9669f1e6&keyword=" + city)
-        # json.dump(response.json(), outFile)
+        # response = req.get("https://api.waqi.info/search/?token=491989f2467ee8ad41a1ed72d33635bf9669f1e6&keyword=" + )
+        # save response it results
+        # 
 
     return {
         'userId': 1, 
         'title': 'Flask React Application', 
         'completed': False
     }
+
+
+
+    
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,62 +1,14 @@
 import React, {useState} from 'react';
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, {Source, Layer} from 'react-map-gl';
 import mapboxgl from 'mapbox-gl';
 
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZGFubnlvaDAzMTYiLCJhIjoiY2tnbjF5enpiMDV3azJ5cWxzcWd5djJ6NCJ9.fN9v1ZMyAVCSIWeITwhg7w';
 
-const heatmapLayer = () => (
-  {
-    id: 'trees-heat',
-    type: 'heatmap',
-    source: 'trees',
-    maxzoom: 15,
-    paint: {
-      // increase weight as diameter breast height increases
-      'heatmap-weight': {
-        property: 'dbh',
-        type: 'exponential',
-        stops: [
-          [1, 0],
-          [62, 1]
-        ]
-      },
-      // increase intensity as zoom level increases
-      'heatmap-intensity': {
-        stops: [
-          [11, 1],
-          [15, 3]
-        ]
-      },
-      // assign color values be applied to points depending on their density
-      'heatmap-color': [
-        'interpolate',
-        ['linear'],
-        ['heatmap-density'],
-        0, 'rgba(236,222,239,0)',
-        0.2, 'rgb(208,209,230)',
-        0.4, 'rgb(166,189,219)',
-        0.6, 'rgb(103,169,207)',
-        0.8, 'rgb(28,144,153)'
-      ],
-      // increase radius as zoom increases
-      'heatmap-radius': {
-        stops: [
-          [11, 15],
-          [15, 20]
-        ]
-      },
-      // decrease opacity to transition into the circle layer
-      'heatmap-opacity': {
-        default: 1,
-        stops: [
-          [14, 1],
-          [15, 0]
-        ]
-      },
-    }
-  }, 'waterway-label'
-)
+const geojson = {
+  type: 'FeatureCollection',
+  features: [{type: 'Feature', properties: {dbh: 0}, geometry: {type: 'Point', coordinates: [40.8069, -73.9619]}}]
+};
 
 const Map = () => {
   const [viewport, setViewport] = useState({
@@ -74,9 +26,13 @@ const Map = () => {
       mapStyle='mapbox://styles/dannyoh0316/ckgn3lw4x1t0u1amnxp3mu92e'
       // mapStyle='mapbox://styles/mapbox/dark-v10'
       onViewportChange={viewport => setViewport(viewport)}
-      addSource={'trees', {type: 'geojson', data: '../trees.geojson'}}
-      addLayer={heatmapLayer}
     >
+      <Source id="my-data" type="geojson" data={geojson}>
+          <Layer
+            id="point"
+            type="heatmap"
+          />
+      </Source>
     </ReactMapGL>
   );
 };
