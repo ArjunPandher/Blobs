9520bfccebc5bd28520d9f4dc52eefce63292758
Danny Oh <dannyoh0316@gmail.com>
2020-10-23 22:41:29 -0500

    map showing

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import mapboxgl from 'mapbox-gl';
 
 function App() {
   const [viewport, setViewport] = useState({
-    latitude: 45.4211,
-    longitude: -75.6903,
+    latitude: 40.8069488,
+    longitude: -73.9618974,
     width: '100vw',
     height: '100vh',
-    zoom: 10
+    zoom: 15
   })
 
   return (
     <ReactMapGL
       {...viewport}
       mapboxApiAccessToken={mapboxgl.accessToken}
+      mapStyle='mapbox://styles/dannyoh0316/ckgn3lw4x1t0u1amnxp3mu92e'
+      // mapStyle='mapbox://styles/mapbox/dark-v10'
+      onViewportChange={viewport => setViewport(viewport)}
     >
       markers here
     </ReactMapGL>
