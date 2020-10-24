bf60a32b16c955b9781810de5161c132822d5de2
An4123 <anvo2016026@gmail.com>
2020-10-24 00:36:42 -0600

    Update api.py

--- api/api.py
+++ api/api.py
@@ -1,21 +1,21 @@
 from flask import Flask
-
+import json
 
 app = Flask(__name__)
 
-@app.route('/api', methods=['GET'])
+@app.route('/', methods=['GET'])
 def api():
-    # given "cities"
-    #[cities]
-
-    # results[]
-    # for city in cities
-        # response = req.get("https://api.waqi.info/search/?token=491989f2467ee8ad41a1ed72d33635bf9669f1e6&keyword=" + )
-        # save response it results
-        # 
-
-    return {
-        'userId': 1, 
-        'title': 'Flask React Application', 
-        'completed': False
-    }
+    states = {}     
+    cities = ["colorado", "new+york", "maine"]
+    for city in cities:
+        thingsWeWant = {}           # [geo, aqi, name]
+        response = req.get("https://api.waqi.info/search/?token=491989f2467ee8ad41a1ed72d33635bf9669f1e6&keyword=" + city)
+        data = response.json()
+        for oneData in data["data"]:
+            aqi, geo, name = oneData["aqi"], oneData["station"]["geo"], oneData["station"]["name"]
+            tempDict = {}
+            tempDict["aqi"], tempDict["geo"], tempDict["name"] = aqi, geo, name
+            thingsWeWant[oneData["uid"]] = tempDict
+        states[city] = thingsWeWant
+    return states
+    
